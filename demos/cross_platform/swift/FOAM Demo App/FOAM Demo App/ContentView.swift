ee8818b7e8af7d2f26182e7d38d027f7c9ac7d6d
Mike Carcasole <mikecarcasole@gmail.com>
2019-11-04 08:41:41 -0500

    Add swift project

---
+++ demos/cross_platform/swift/FOAM Demo App/FOAM Demo App/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  FOAM Demo App
+//
+//  Created by Michael Carcasole on 2019-11-03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ demos/cross_platform/swift/FOAM Demo App/FOAM Demo AppTests/FOAM_Demo_AppTests.swift
@@ -0,0 +1,33 @@
+//
+//  FOAM_Demo_AppTests.swift
+//  FOAM Demo AppTests
+//
+//  Created by Michael Carcasole on 2019-11-03.
+//
+
+import XCTest
+@testable import FOAM_Demo_App
+
+class FOAM_Demo_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
