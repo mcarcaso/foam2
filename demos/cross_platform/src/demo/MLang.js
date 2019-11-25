62e63f1288c3ba0ab862bfecb0f61f17b3e7e74e
Mike Carcasole <mikecarcasole@gmail.com>
2019-11-25 13:46:39 -0500

    Support ArraySink on android and iOS

--- demos/cross_platform/src/demo/MLang.js
+++ demos/cross_platform/src/demo/MLang.js
@@ -2,6 +2,8 @@ foam.CLASS({
   package: 'demo',
   name: 'MLang',
   requires: [
+    'demo.Person',
+    'foam.dao.ArraySink',
     'foam.mlang.sink.Count',
   ],
   properties: [
@@ -11,6 +13,13 @@ foam.CLASS({
       required: true,
       name: 'countSink',
       crossPlatformFactory: `return Count_create().build();`,
+    },
+    {
+      class: 'FObjectProperty',
+      of: 'foam.dao.ArraySink',
+      required: true,
+      name: 'arraySink',
+      crossPlatformFactory: `return ArraySink_create().build();`,
     },
   ],
   tests: [
@@ -29,6 +38,53 @@ foam.CLASS({
         <%=assertEqual(3, 'o.getCountSink().getValue()')%>
         o.getCountSink().reset(<%=nul()%>);
         <%=assertEqual(0, 'o.getCountSink().getValue()')%>
+      `
+    },
+    {
+      name: 'testArraySink',
+      androidCode: `
+        demo.MLang o = MLang_create().build();
+
+        demo.Person p1 = o.Person_create()
+          .setFirstName("A")
+          .build();
+        demo.Person p2 = o.Person_create()
+          .setFirstName("B")
+          .build();
+
+        o.getArraySink().put(p1, null);
+        assertEquals(1, o.getArraySink().getArray().size());
+
+        o.getArraySink().put(p2, null);
+        assertEquals(2, o.getArraySink().getArray().size());
+
+        o.getArraySink().remove(p1, null);
+        assertEquals(1, o.getArraySink().getArray().size());
+
+        o.getArraySink().reset(null);
+        assertEquals(0, o.getArraySink().getArray().size());
+      `,
+      swiftCode: `
+        let o = MLang_create().build();
+
+        let p1 = o.Person_create()
+          .setFirstName("A")
+          .build();
+        let p2 = o.Person_create()
+          .setFirstName("B")
+          .build();
+
+        o.getArraySink().put(p1, nil);
+        XCTAssertEqual(1, o.getArraySink().getArray().count);
+
+        o.getArraySink().put(p2, nil);
+        XCTAssertEqual(2, o.getArraySink().getArray().count);
+
+        o.getArraySink().remove(p1, nil);
+        XCTAssertEqual(1, o.getArraySink().getArray().count);
+
+        o.getArraySink().reset(nil);
+        XCTAssertEqual(0, o.getArraySink().getArray().count);
       `
     },
   ]
