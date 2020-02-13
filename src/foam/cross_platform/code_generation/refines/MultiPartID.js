503241e6652805ca6329f8fd7966018ce24be5ff
Mike Carcasole <mikecarcasole@gmail.com>
2020-02-13 17:41:19 -0500

    Android multipartid

--- src/foam/cross_platform/code_generation/refines/MultiPartID.js
+++ src/foam/cross_platform/code_generation/refines/MultiPartID.js
@@ -2,13 +2,26 @@ foam.CLASS({
   package: 'foam.cross_platform.code_generation.refines',
   name: 'MultiPartID',
   refines: 'foam.core.MultiPartID',
-  flags: ['swift'],
   properties: [
     {
       name: 'expressionArgs',
       factory: function() {
         return this.propNames
       },
+    },
+    ['androidGetter', null],
+    {
+      name: 'androidExpression',
+      expression: function(of, propNames) {
+        return `
+          return (${of.id}) ${of.id}.CLS_()
+            .createBuilder(getSubX())
+          ${propNames.map(p => `
+            .setBuilderProperty("${p}", getProperty("${p}"))
+          `).join('')}
+            .builderBuild();
+        `
+      }
     },
     {
       name: 'swiftExpression',
