db81fb1e8e308fc1e598114dbb30722cf59f9c70
Mike Carcasole <mikecarcasole@gmail.com>
2020-03-29 13:44:40 -0400

    Create a CodeSource axiom that'll create a source file and plop it in the given path

---
+++ src/foam/cross_platform/code_generation/CodeSource.js
@@ -0,0 +1,23 @@
+foam.CLASS({
+  package: 'foam.cross_platform.code_generation',
+  name: 'CodeSource',
+  properties: [
+    {
+      name: 'name',
+      expression: function(path) { return path }
+    },
+    'flags',
+    'body',
+    'path',
+  ],
+  methods: [
+    function buildAndroidResources(resources, parentCls) {
+      resources.sources.push({
+        toSource: () => ({
+          body: this.body,
+          path: this.path,
+        })
+      });
+    },
+  ]
+});
