e50f81392ee46b9290e7f01e354150a229eabdc9
Mike Carcasole <mikecarcasole@gmail.com>
2020-04-09 09:45:02 -0400

    Add swift support to CodeSource

--- src/foam/cross_platform/code_generation/CodeSource.js
+++ src/foam/cross_platform/code_generation/CodeSource.js
@@ -19,5 +19,13 @@ foam.CLASS({
         })
       });
     },
+    function buildSwiftResources(resources, parentCls) {
+      resources.sources.push({
+        toSource: () => ({
+          body: this.body,
+          path: this.path,
+        })
+      });
+    },
   ]
 });
